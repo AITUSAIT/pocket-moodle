e4a785dff341d9b043527d0ed2b9ed636e0cf549
DAKExDUCK <arsengabdulin228@gmail.com>
2022-10-07 18:21:20 +0600

    fix

--- app/api/router.py
+++ app/api/router.py
@@ -10,7 +10,7 @@ from bot.objects.logger import logger
 from bot.objects import aioredis
 from robokassa import result_payment
 
-users = [1709219105]
+users = []
 start_time = None
 
 
@@ -19,15 +19,15 @@ async def get_user(request):
     global start_time
     if request.rel_url.query.get('token', None) in tokens:
         while 1:
-            # if len(users) == 0:
-            #     if start_time is not None:
-            #         logger.info(f"{(time.time() - start_time)} секунд\n")
-            #     start_time = time.time()
-            #     users = await aioredis.redis.keys()
-            #     users.sort()
-            #     users.remove('news')
+            if len(users) == 0:
+                if start_time is not None:
+                    logger.info(f"{(time.time() - start_time)} секунд\n")
+                start_time = time.time()
+                users = await aioredis.redis.keys()
+                users.sort()
+                users.remove('news')
             user = await aioredis.get_dict(users[0])
-            # del users[0]
+            del users[0]
             user['courses'] = json.loads(user.get('courses', '{}'))
             user['gpa'] = json.loads(user.get('gpa', '{}'))
             user['att_statistic'] = json.loads(user.get('att_statistic', '{}'))
