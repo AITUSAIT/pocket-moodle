81fbe2cea50749d870543cb3e5716fc7595fc064
DAKExDUCK <arsengabdulin228@gmail.com>
2022-10-14 15:18:25 +0600

    not test payment

--- bot/handlers/purchase.py
+++ bot/handlers/purchase.py
@@ -50,7 +50,7 @@ async def create_payment(query: types.CallbackQuery, state: FSMContext):
         merchant_password_1=robo_passwd_1,
         cost=cost,
         number=id,
-        description=f"Покупка подписки. Цена:{cost}тг. Период:{months}"
+        description=f"Покупка подписки на {months} месяц(-ев)"
     )
     text = "Payment link is ready!"
     kb = payment_btn(link)
