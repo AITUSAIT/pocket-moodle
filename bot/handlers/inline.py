dcd79a6e24b6f7b954fe580fb2b859cbe0a6b2c3
DAKExDUCK <arsengabdulin228@gmail.com>
2022-10-23 11:05:05 +0600

    add gpa

--- bot/handlers/inline.py
+++ bot/handlers/inline.py
@@ -12,27 +12,97 @@ from bot.objects import aioredis
 
 async def show_courses_list(inline_query: types.InlineQuery):
     user_id = inline_query.from_user.id
-    courses = json.loads(await aioredis.get_key(user_id, 'courses'))
-    url = 'https://moodle.astanait.edu.kz/mod/assign/view.php?id='
-    url_course = 'https://moodle.astanait.edu.kz/course/view.php?id='
-
     results = []
-    if len(inline_query.query) == 0:
-        for course in courses.values():
-            if not course['active']:
-                continue
 
-            course_name = course['name']
-            if len(course_name.split('|')) == 2:
-                course_name = ' | '.join(course_name.split('|')[:1] + course_name.split('|')[2:])
+    if await aioredis.is_active_sub(user_id):
+        courses = json.loads(await aioredis.get_key(user_id, 'courses'))
+        url = 'https://moodle.astanait.edu.kz/mod/assign/view.php?id='
+        url_course = 'https://moodle.astanait.edu.kz/course/view.php?id='
 
-            course_id = course['id']
-            grades_text = f"[{clear_MD(course_name)}]({clear_MD(f'{url_course}{course_id}')})\n"
-            for grade in course['grades'].values():
-                name = grade['name']
-                percentage = grade['percentage']
-                grades_text += f"    {clear_MD(name)}  \-  {clear_MD(percentage)}\n"
-            results.append(
+        if len(inline_query.query) == 0:
+            active_courses = list(course for course in courses.values() if course['active'])
+
+            if await aioredis.is_ready_gpa(user_id):
+                gpa_text = await aioredis.get_gpa_text(user_id)
+                results.append(
+                    types.InlineQueryResultArticle(
+                        id='gpa',
+                        title='GPA',
+                        input_message_content=types.InputTextMessageContent(
+                            gpa_text,
+                            parse_mode='MarkdownV2'
+                        )
+                    )
+                )
+
+
+            for course in active_courses:
+                course_name = course['name']
+                if len(course_name.split('|')) == 2:
+                    course_name = ' | '.join(course_name.split('|')[:1] + course_name.split('|')[2:])
+
+                course_id = course['id']
+                grades_text = f"[{clear_MD(course_name)}]({clear_MD(f'{url_course}{course_id}')})\n"
+                for grade in course['grades'].values():
+                    name = grade['name']
+                    percentage = grade['percentage']
+                    grades_text += f"    {clear_MD(name)}  \-  {clear_MD(percentage)}\n"
+                results.append(
+                    types.InlineQueryResultArticle(
+                        id=course_id,
+                        title=course_name + ' | Grades',
+                        input_message_content=types.InputTextMessageContent(
+                            grades_text,
+                            parse_mode='MarkdownV2'
+                        )
+                    )
+                )
+
+                if course['active']:
+                    assign_text = f"[{clear_MD(course_name)}]({clear_MD(f'{url_course}{course_id}')})\n"
+                    assign_state = False
+                    for assign in course['assignments'].values():
+                        name = assign['name']
+                        due = assign['due']
+                        diff_time = get_diff_time(due)
+                        if diff_time>timedelta(days=0):
+                            assign_text += f"\n    [{clear_MD(name)}]({clear_MD(url+assign['id'])})"
+                            assign_text += f"\n    {clear_MD(due)}"
+                            assign_text += f"\n    Remaining: {clear_MD(diff_time)}"
+                            assign_text += '\n'
+                            assign_state = True
+                    if assign_state:
+                        results.append(
+                            types.InlineQueryResultArticle(
+                                id=str(course_id)+'_assign',
+                                title=course_name + ' | Deadlines',
+                                input_message_content=types.InputTextMessageContent(
+                                    assign_text,
+                                    parse_mode='MarkdownV2'
+                                )
+                            )
+                        )
+        else:
+            courses_names = []
+            for course in courses.values():
+                course_name = course['name']
+                courses_names.append(course_name)
+
+            sorted_names = process.extract(inline_query.query, courses_names)
+
+            for course_name, res in sorted_names:
+                course = list(course for course in courses.values() if course['name'] == course_name)[0]
+                course_id = course['id']
+                if len(course_name.split('|')) == 2:
+                    course_name = ' | '.join(course_name.split('|')[:1] + course_name.split('|')[2:])
+
+
+                grades_text = f"[{clear_MD(course_name)}]({clear_MD(f'{url_course}{course_id}')})\n"
+                for grade in course['grades'].values():
+                    name = grade['name']
+                    percentage = grade['percentage']
+                    grades_text += f"    {clear_MD(name)}  \-  {clear_MD(percentage)}\n"
+                results.append(
                 types.InlineQueryResultArticle(
                     id=course_id,
                     title=course_name + ' | Grades',
@@ -41,87 +111,44 @@ async def show_courses_list(inline_query: types.InlineQuery):
                             parse_mode='MarkdownV2'
                         )
                     )
-            )
+                )
 
-            if course['active']:
-                assign_text = f"[{clear_MD(course_name)}]({clear_MD(f'{url_course}{course_id}')})\n"
-                assign_state = False
-                for assign in course['assignments'].values():
-                    name = assign['name']
-                    due = assign['due']
-                    diff_time = get_diff_time(due)
-                    if diff_time>timedelta(days=0):
-                        assign_text += f"\n    [{clear_MD(name)}]({clear_MD(url+assign['id'])})"
-                        assign_text += f"\n    {clear_MD(due)}"
-                        assign_text += f"\n    Remaining: {clear_MD(diff_time)}"
-                        assign_text += '\n'
-                        assign_state = True
-                if assign_state:
-                    results.append(
-                        types.InlineQueryResultArticle(
-                            id=str(course_id)+'_assign',
-                            title=course_name + ' | Deadlines',
-                            input_message_content=types.InputTextMessageContent(
-                                assign_text,
-                                parse_mode='MarkdownV2'
+                if course['active']:
+                    assign_text = f"[{clear_MD(course_name)}]({clear_MD(f'{url_course}{course_id}')})\n"
+                    assign_state = False
+                    for assign in course['assignments'].values():
+                        name = assign['name']
+                        due = assign['due']
+                        diff_time = get_diff_time(due)
+                        if diff_time>timedelta(days=0):
+                            assign_text += f"\n    [{clear_MD(name)}]({clear_MD(url+assign['id'])})"
+                            assign_text += f"\n    {clear_MD(due)}"
+                            assign_text += f"\n    Remaining: {clear_MD(diff_time)}"
+                            assign_text += '\n'
+                            assign_state = True
+                    if assign_state:
+                        results.append(
+                            types.InlineQueryResultArticle(
+                                id=str(course_id)+'_assign',
+                                title=course_name + ' | Deadlines',
+                                input_message_content=types.InputTextMessageContent(
+                                    assign_text,
+                                    parse_mode='MarkdownV2'
+                                )
                             )
                         )
-                    )
     else:
-        courses_names = []
-        for course in courses.values():
-            course_name = course['name']
-            courses_names.append(course_name)
-
-        sorted_names = process.extract(inline_query.query, courses_names)
-
-        for course_name, res in sorted_names:
-            course = list(course for course in courses.values() if course['name'] == course_name)[0]
-            course_id = course['id']
-            if len(course_name.split('|')) == 2:
-                course_name = ' | '.join(course_name.split('|')[:1] + course_name.split('|')[2:])
-
-
-            grades_text = f"[{clear_MD(course_name)}]({clear_MD(f'{url_course}{course_id}')})\n"
-            for grade in course['grades'].values():
-                name = grade['name']
-                percentage = grade['percentage']
-                grades_text += f"    {clear_MD(name)}  \-  {clear_MD(percentage)}\n"
-            results.append(
+        text = 'Your subscription is not active'
+        results.append(
             types.InlineQueryResultArticle(
-                id=course_id,
-                title=course_name + ' | Grades',
+                id='error',
+                title='Your sub is not active',
                 input_message_content=types.InputTextMessageContent(
-                        grades_text,
-                        parse_mode='MarkdownV2'
-                    )
+                    text,
+                    parse_mode='MarkdownV2'
                 )
             )
-
-            if course['active']:
-                assign_text = f"[{clear_MD(course_name)}]({clear_MD(f'{url_course}{course_id}')})\n"
-                assign_state = False
-                for assign in course['assignments'].values():
-                    name = assign['name']
-                    due = assign['due']
-                    diff_time = get_diff_time(due)
-                    if diff_time>timedelta(days=0):
-                        assign_text += f"\n    [{clear_MD(name)}]({clear_MD(url+assign['id'])})"
-                        assign_text += f"\n    {clear_MD(due)}"
-                        assign_text += f"\n    Remaining: {clear_MD(diff_time)}"
-                        assign_text += '\n'
-                        assign_state = True
-                if assign_state:
-                    results.append(
-                        types.InlineQueryResultArticle(
-                            id=str(course_id)+'_assign',
-                            title=course_name + ' | Deadlines',
-                            input_message_content=types.InputTextMessageContent(
-                                assign_text,
-                                parse_mode='MarkdownV2'
-                            )
-                        )
-                    )
+        )
 
     await inline_query.answer(results[:50], is_personal=True, cache_time=5)
 
