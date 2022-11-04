b4b6063917850846c8fc226e271f6701430ab0bd
DAKExDUCK <arsengabdulin228@gmail.com>
2022-11-04 13:27:27 +0600

    fix api

--- app/api/router.py
+++ app/api/router.py
@@ -33,8 +33,8 @@ async def get_user(request):
             user['gpa'] = json.loads(user.get('gpa', '{}'))
             user['att_statistic'] = json.loads(user.get('att_statistic', '{}'))
 
-            if not user.get('user_id', None):
-                break
+            if user.get('user_id', None):
+                continue
 
             if await aioredis.is_sleep(user['user_id']):
                 continue
