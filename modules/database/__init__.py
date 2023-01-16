0843a808b9f9e9719fc7a127630f8f01082d6478
DAKExDUCK <arsengabdulin228@gmail.com>
2023-01-16 19:23:08 +0600

    fix tabs

--- modules/database/__init__.py
+++ modules/database/__init__.py
@@ -19,26 +19,33 @@ def clear_MD(text: str) -> str:
 redis: Redis = None
 redis1: Redis = None
 
+
 async def start_redis(user: str, passwd: str, host: str, port: str, db: str):
     global redis
     redis = await aioredis.from_url(f"redis://{user}:{passwd}@{host}:{port}/{db}", decode_responses=True)
     global redis1
     redis1 = await aioredis.from_url(f"redis://{user}:{passwd}@{host}:{port}/1", decode_responses=True)
 
+
 async def set_key(name: str, key: str, value):
     await redis.hset(name, key, value)
+
 
 async def get_key(name: str, key: str):
     return await redis.hget(name, key)
 
+
 async def set_keys(name: str, dict: dict):
     await redis.hmset(name, dict)
+
 
 async def get_keys(name: str, *keys) -> tuple:
     return await redis.hmget(name, *keys)
 
+
 async def get_dict(name: str) -> dict:
     return await redis.hgetall(name)
+
 
 async def if_user(user_id: int) -> bool:
     if await redis.exists(user_id) == 0:
@@ -46,11 +53,13 @@ async def if_user(user_id: int) -> bool:
     else:
         return True
 
+
 async def is_registered_moodle(user_id: int) -> bool:
     if await redis.hget(user_id, 'barcode'):
         return True
     else:
         return False
+
 
 async def new_user(user_id: int):
     new_user = {
@@ -60,6 +69,7 @@ async def new_user(user_id: int):
     }
 
     await set_keys(user_id, new_user)
+
 
 async def activate_subs(user_id: int, days: int):
     user = {}
@@ -71,6 +81,7 @@ async def activate_subs(user_id: int, days: int):
     user['message_end_date'] = 0
 
     await set_keys(user_id, user)
+
 
 async def is_new_user(user_id: int) -> bool:
     if not await if_user(user_id):
@@ -86,6 +97,7 @@ async def is_new_user(user_id: int) -> bool:
     else:
         return False
 
+
 async def is_active_sub(user_id: int) -> bool:
     if not await if_user(user_id):
         return False
@@ -99,6 +111,7 @@ async def is_active_sub(user_id: int) -> bool:
         return True
     else:
         return False
+
 
 async def is_ready_courses(user_id: int) -> bool:
     if await if_user(user_id):
@@ -129,6 +142,7 @@ async def is_ready_curriculum(user_id: int) -> bool:
     else:
         return False
 
+
 async def is_ready_gpa(user_id: int) -> bool:
     if await if_user(user_id):
         if await redis.hexists(user_id, 'gpa') == 1:
@@ -137,6 +151,7 @@ async def is_ready_gpa(user_id: int) -> bool:
             return False
     else:
         return False
+
 
 async def is_sleep(user_id: int) -> bool:
     if await if_user(user_id):
@@ -150,6 +165,7 @@ async def is_sleep(user_id: int) -> bool:
     else:
         return False
 
+
 async def get_gpa_text(user_id: int) -> str:
     gpa_dict = json.loads(await redis.hget(user_id, 'gpa'))
     
@@ -158,6 +174,7 @@ async def get_gpa_text(user_id: int) -> str:
         text += f"{clear_MD(key)} \- *{clear_MD(gpa_dict[key])}*\n"
     
     return text
+
 
 async def user_register_moodle(user_id: int, barcode: str, passwd: str):
     user = {}
@@ -169,6 +186,7 @@ async def user_register_moodle(user_id: int, barcode: str, passwd: str):
     await redis.hdel(user_id, 'att_statistic', 'gpa', 'courses', 'cookies', 'token', 'message', 'message_end_date')
     await set_keys(user_id, user)
 
+
 async def check_if_msg_end_date(user_id: int) -> int:
     if not await redis.hexists(user_id, 'message_end_date'):
         return False
@@ -176,11 +194,14 @@ async def check_if_msg_end_date(user_id: int) -> int:
     message = int(await redis.hget(user_id, 'message_end_date'))
     return message
 
+
 async def set_msg_end_date(user_id: int, number: int):
     await redis.hset(user_id, 'message_end_date', number)
 
+
 async def close():
     await redis.close()
+
 
 def crypto(message: str, secret: str) -> str:
     new_chars = list()
@@ -193,8 +214,10 @@ def crypto(message: str, secret: str) -> str:
             i = 0
     return ''.join(chr(c) for c in new_chars)
 
+
 def crypt(message: str, secret: str) -> str:
     return crypto(message, secret).encode('utf-8').hex()
+
 
 def decrypt(message_hex: str, secret: str) -> str:
     message = bytes.fromhex(message_hex).decode('utf-8')
