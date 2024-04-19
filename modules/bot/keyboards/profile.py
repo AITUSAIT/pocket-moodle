21ba613d2542ac8badad55000712aeed171aa1e4
AMD_RYZEN_5_7600X <arca26100@gmail.com>
2024-04-19 18:08:04 +0500

    changed profile keyboard construction

--- modules/bot/keyboards/profile.py
+++ modules/bot/keyboards/profile.py
@@ -1,11 +1,12 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def profile_btns(kb: InlineKeyboardMarkup | None = None) -> InlineKeyboardMarkup:
+def profile_btns(kb: InlineKeyboardBuilder | None = None) -> InlineKeyboardBuilder:
     if kb is None:
-        kb = InlineKeyboardMarkup()
+        kb = InlineKeyboardBuilder()
 
-    main_menu = InlineKeyboardButton("Back", callback_data="main_menu")
+    main_menu = InlineKeyboardButton(text="Back", callback_data="main_menu")
     kb.row(main_menu)
 
     return kb
