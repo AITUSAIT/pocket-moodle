621bd5e1df9658d48857c9e0400a61c067e3ca64
amdRyzen5600X <arca26100@gmail.com>
2024-06-16 14:44:10 +0500

    FIX:  handlers moodle;

--- modules/bot/handlers/moodle.py
+++ modules/bot/handlers/moodle.py
@@ -1,8 +1,11 @@
 from aiogram import F, Router, types
+from aiogram.enums.parse_mode import ParseMode
 from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.types.message import Message
 
+import global_vars
 from config import ENHANCED_SCHOLARSHIP_THRESHOLD, RATE, SCHOLARSHIP_THRESHOLD
 from modules.bot.functions.deadlines import get_deadlines_local_by_course, get_deadlines_local_by_days
 from modules.bot.functions.functions import (
@@ -50,6 +53,8 @@ class Submit(StatesGroup):
 @rate_limit(limit=RATE)
 @Logger.log_msg
 async def register_moodle_query(query: types.CallbackQuery, state: FSMContext):
+    if not isinstance(query.message, Message):
+        return
     await query.answer()
 
     user_id = query.from_user.id
@@ -59,18 +64,19 @@ async def register_moodle_query(query: types.CallbackQuery, state: FSMContext):
 
     msg = await query.message.answer(
         f"Write your *Barcode* or *Email address* from [here]({escape_md('https://moodle.astanait.edu.kz/user/profile.php')}):",
-        parse_mode=types.ParseMode.MARKDOWN_V2,
+        parse_mode=ParseMode.MARKDOWN_V2
     )
-    await delete_msg(query.message)
-    await MoodleForm.wait_mail.set()
 
-    async with state.proxy() as data:
-        data["msg_del"] = msg
+    await delete_msg(query.message)
+    await state.set_state(MoodleForm.wait_mail)
+    await state.set_data({"msg_de": msg})
 
 
 @rate_limit(limit=RATE)
 @Logger.log_msg
 async def register(message: types.Message, state: FSMContext):
+    if message.from_user is None:
+        return
     user_id = message.from_user.id
     user = await UserDB.get_user(user_id)
     if not user:
@@ -78,17 +84,17 @@ async def register(message: types.Message, state: FSMContext):
 
     msg = await message.answer(
         f"Write your *Barcode* or *Email address* from [here]({escape_md('https://moodle.astanait.edu.kz/user/profile.php')}):",
-        parse_mode=types.ParseMode.MARKDOWN_V2,
+        parse_mode=ParseMode.MARKDOWN_V2,
     )
     await delete_msg(message)
-    await MoodleForm.wait_mail.set()
-
-    async with state.proxy() as data:
-        data["msg_del"] = msg
+    await state.set_state(MoodleForm.wait_mail)
+    await state.set_data({"msg_de": msg})
 
 
 @rate_limit(limit=RATE)
 async def wait_mail(message: types.Message, state: FSMContext):
+    if message.text is None:
+        return
     if check_is_valid_mail(message.text):
         mail = message.text
     elif message.text.isnumeric():
@@ -96,34 +102,40 @@ async def wait_mail(message: types.Message, state: FSMContext):
     else:
         msg = await message.answer(
             f"*Email* or *Barcode* not valid, try again❗️\n\nWrite your *Email address* from [here]({escape_md('https://moodle.astanait.edu.kz/user/profile.php')}):",
-            parse_mode=types.ParseMode.MARKDOWN_V2,
+            parse_mode=ParseMode.MARKDOWN_V2,
         )
 
-        async with state.proxy() as data:
-            await delete_msg(data["msg_del"], message)
-            data["msg_del"] = msg
+        data = await state.get_data()
+        await delete_msg(data["msg_del"], message)
+        data["msg_del"] = msg
+        await state.set_data(data)
         return
 
     msg = await message.answer(
         f"Write your *Moodle mobile web service* Key from [here]({escape_md('https://moodle.astanait.edu.kz/user/managetoken.php')}):",
-        parse_mode=types.ParseMode.MARKDOWN_V2,
+        parse_mode=ParseMode.MARKDOWN_V2,
     )
-    await MoodleForm.wait_api_token.set()
+    await state.set_state(MoodleForm.wait_api_token)
 
-    async with state.proxy() as data:
-        await delete_msg(data["msg_del"], message)
-        data["msg_del"] = msg
-        data["mail"] = mail
+    data = await state.get_data()
+    await delete_msg(data["msg_del"], message)
+    data["msg_del"] = msg
+    data["mail"] = mail
+    await state.set_data(data)
 
 
 @rate_limit(limit=RATE)
 @count_active_user
 async def wait_api_token(message: types.Message, state: FSMContext):
+    if message.from_user is None:
+        return
+    if message.text is None:
+        return
     user_id = message.from_user.id
     api_token = message.text
-    async with state.proxy() as data:
-        await delete_msg(data["msg_del"], message)
-        mail = data["mail"]
+    data = await state.get_data()
+    await delete_msg(data["msg_del"], message)
+    mail = data["mail"]
 
     try:
         await MoodleAPI.check_api_token(mail, api_token)
@@ -141,16 +153,17 @@ async def wait_api_token(message: types.Message, state: FSMContext):
 
         text = "Your Moodle account is registered\!"
 
-        await message.answer(text, parse_mode=types.ParseMode.MARKDOWN_V2, reply_markup=main_menu().as_markup())
-        await state.finish()
+        await message.answer(text, parse_mode=ParseMode.MARKDOWN_V2, reply_markup=main_menu().as_markup())
+        await state.clear()
         return
 
-    msg = await message.answer(text, parse_mode=types.ParseMode.MARKDOWN_V2)
-    await state_to_set.set()
-    async with state.proxy() as data:
-        await delete_msg(data["msg_del"], message)
-        data["msg_del"] = msg
-        mail = data["mail"]
+    msg = await message.answer(text, parse_mode=ParseMode.MARKDOWN_V2)
+    await state.set_state(state_to_set)
+    data = await state.get_data()
+    await delete_msg(data["msg_del"], message)
+    data["msg_del"] = msg
+    mail = data["mail"]
+    await state.set_data(data)
 
 
 @rate_limit(limit=RATE)
@@ -158,6 +171,8 @@ async def wait_api_token(message: types.Message, state: FSMContext):
 @count_active_user
 @login_required
 async def get_grades(query: types.CallbackQuery):
+    if not isinstance(query.message, Message):
+        return
     if not await CourseDB.is_ready_courses(query.from_user.id):
         text = "Your courses are not ready, you are in queue, try later. If there will be some error, we will notify"
         await query.message.edit_text(text, reply_markup=main_menu().as_markup())
@@ -171,6 +186,10 @@ async def get_grades(query: types.CallbackQuery):
 @count_active_user
 @login_required
 async def get_grades_choose_course_text(query: types.CallbackQuery):
+    if query.data is None:
+        return
+    if not isinstance(query.message, Message):
+        return
     user_id = query.from_user.id
     is_active = query.data.split()[1] == "active"
     text = "Choose one:"
@@ -183,6 +202,10 @@ async def get_grades_choose_course_text(query: types.CallbackQuery):
 @count_active_user
 @login_required
 async def get_grades_course_text(query: types.CallbackQuery):
+    if not isinstance(query.message, Message):
+        return
+    if query.data is None:
+        return
     user_id = query.from_user.id
 
     is_active = query.data.split()[1] == "active"
@@ -199,7 +222,7 @@ async def get_grades_course_text(query: types.CallbackQuery):
         text += f"    {escape_md(name)}  \-  {percentage}\n"
 
     kb = course_back(is_active)
-    await query.message.edit_text(text, reply_markup=kb.as_markup(), parse_mode=types.ParseMode.MARKDOWN_V2)
+    await query.message.edit_text(text, reply_markup=kb.as_markup(), parse_mode=ParseMode.MARKDOWN_V2)
 
 
 @rate_limit(limit=RATE)
@@ -207,6 +230,8 @@ async def get_grades_course_text(query: types.CallbackQuery):
 @Logger.log_msg
 @login_required
 async def get_deadlines(query: types.CallbackQuery):
+    if not isinstance(query.message, Message):
+        return
     if not await CourseDB.is_ready_courses(query.from_user.id):
         text = "Your courses are not ready, you are in queue, try later. If there will be some error, we will notify"
         await query.message.edit_text(text, reply_markup=main_menu().as_markup())
@@ -221,6 +246,8 @@ async def get_deadlines(query: types.CallbackQuery):
 @count_active_user
 @login_required
 async def get_deadlines_choose_courses(query: types.CallbackQuery):
+    if not isinstance(query.message, Message):
+        return
     user_id = query.from_user.id
 
     courses = await CourseDB.get_courses(user_id, True)
@@ -234,6 +261,10 @@ async def get_deadlines_choose_courses(query: types.CallbackQuery):
 @count_active_user
 @login_required
 async def get_deadlines_course(query: types.CallbackQuery):
+    if query.data is None:
+        return
+    if not isinstance(query.message, Message):
+        return
     user_id = query.from_user.id
 
     user = await UserDB.get_user(user_id)
@@ -246,7 +277,7 @@ async def get_deadlines_course(query: types.CallbackQuery):
         text = "So far there are no such"
 
     await query.message.edit_text(
-        text, parse_mode=types.ParseMode.MARKDOWN_V2, reply_markup=deadlines_courses_back_btns().as_markup()
+        text, parse_mode=ParseMode.MARKDOWN_V2, reply_markup=deadlines_courses_back_btns().as_markup()
     )
     await query.answer()
 
@@ -256,6 +287,8 @@ async def get_deadlines_course(query: types.CallbackQuery):
 @count_active_user
 @login_required
 async def get_deadlines_choose_days(query: types.CallbackQuery):
+    if not isinstance(query.message, Message):
+        return
     text = "Choose filter for deadlines:"
     await query.message.edit_text(text, reply_markup=deadlines_days_btns().as_markup())
 
@@ -265,6 +298,10 @@ async def get_deadlines_choose_days(query: types.CallbackQuery):
 @count_active_user
 @login_required
 async def get_deadlines_days(query: types.CallbackQuery):
+    if not isinstance(query.message, Message):
+        return
+    if query.data is None:
+        return
     user_id = query.from_user.id
 
     user = await UserDB.get_user(user_id)
@@ -277,7 +314,7 @@ async def get_deadlines_days(query: types.CallbackQuery):
         text = "So far there are no such"
 
     await query.message.edit_text(
-        text, parse_mode=types.ParseMode.MARKDOWN_V2, reply_markup=deadlines_days_back_btns().as_markup()
+        text, parse_mode=ParseMode.MARKDOWN_V2, reply_markup=deadlines_days_back_btns().as_markup()
     )
     await query.answer()
 
@@ -289,35 +326,46 @@ async def get_deadlines_days(query: types.CallbackQuery):
 async def submit_assign_show_courses(query: types.CallbackQuery | types.Message):
     text = "Choose one:"
     if isinstance(query, types.CallbackQuery):
+        if not isinstance(query.message, Message):
+            return
         user_id = query.from_user.id
 
         courses = await CourseDB.get_courses(user_id, is_active=True)
 
         await query.message.edit_text(text, reply_markup=show_courses_for_submit(courses).as_markup())
     elif isinstance(query, types.Message):
-        message = query
-        user_id = message.from_user.id
+        if query.from_user is None:
+            return
+        user_id = query.from_user.id
 
         courses = await CourseDB.get_courses(user_id, is_active=True)
 
-        await message.answer(text, reply_markup=show_courses_for_submit(courses).as_markup())
+        await query.answer(text, reply_markup=show_courses_for_submit(courses).as_markup())
 
 
 @rate_limit(limit=RATE)
 @login_required
 async def submit_assign_cancel(query: types.CallbackQuery, state: FSMContext):
+    if not isinstance(query.message, Message):
+        return
+    if query.data is None:
+        return
     user_id = query.from_user.id
 
     courses = await CourseDB.get_courses(user_id, True)
 
     text = "Choose one:"
     await query.message.edit_text(text, reply_markup=show_courses_for_submit(courses).as_markup())
-    await state.finish()
+    await state.clear()
 
 
 @rate_limit(limit=RATE)
 @login_required
 async def submit_assign_show_assigns(query: types.CallbackQuery):
+    if not isinstance(query.message, Message):
+        return
+    if query.data is None:
+        return
     user_id = query.from_user.id
     course_id = query.data.split()[1]
 
@@ -331,6 +379,10 @@ async def submit_assign_show_assigns(query: types.CallbackQuery):
 @rate_limit(limit=RATE)
 @login_required
 async def submit_assign_choose_type(query: types.CallbackQuery):
+    if not isinstance(query.message, Message):
+        return
+    if query.data is None:
+        return
     course_id = query.data.split()[1]
     assign_id = query.data.split()[2]
 
@@ -340,38 +392,47 @@ async def submit_assign_choose_type(query: types.CallbackQuery):
 @rate_limit(limit=RATE)
 @login_required
 async def submit_assign_wait(query: types.CallbackQuery, state: FSMContext):
+    if not isinstance(query.message, Message):
+        return
+    if query.data is None:
+        return
     course_id = query.data.split()[1]
     assign_id = query.data.split()[2]
     submit_type = query.data.split()[3]
 
+    text = ""
     if submit_type == "file":
         text = "Send file as document for submit (support only one)"
-        await Submit.wait_file.set()
+        await state.set_state(Submit.wait_file)
     elif submit_type == "text":
         text = "Send text for submit"
-        await Submit.wait_text.set()
+        await state.set_state(Submit.wait_text)
 
     msg = await query.message.edit_text(text, reply_markup=show_assigns_cancel_btn().as_markup())
 
-    async with state.proxy() as data:
-        data["course_id"] = course_id
-        data["assign_id"] = assign_id
-        data["type"] = submit_type
-        data["msg"] = msg
+    data = await state.get_data()
+    data["course_id"] = course_id
+    data["assign_id"] = assign_id
+    data["type"] = submit_type
+    data["msg"] = msg
+    await state.set_data(data)
 
 
 @rate_limit(limit=RATE)
 @count_active_user
 @login_required
 async def submit_assign_file(message: types.Message, state: FSMContext):
+    if message.from_user is None:
+        return
     user_id = message.from_user.id
     user = await UserDB.get_user(user_id)
     if not user:
         return
 
-    async with state.proxy() as data:
-        course_id = data["course_id"]
-        assign_id = data["assign_id"]
+    data = await state.get_data()
+    course_id = data["course_id"]
+    assign_id = data["assign_id"]
+    await state.set_data(data)
 
     courses = await CourseDB.get_courses(user_id, True)
     course = courses[str(course_id)]
@@ -380,14 +441,20 @@ async def submit_assign_file(message: types.Message, state: FSMContext):
     url_to_course = f"https://moodle.astanait.edu.kz/course/view.php?id={course.course_id}"
     url_to_assign = f"https://moodle.astanait.edu.kz/mod/assign/view.php?id={assign.id}"
 
+    if message.document is None:
+        return
     file_id = message.document.file_id
-    file = await message.bot.get_file(file_id)
+    file = await global_vars.bot.get_file(file_id)
     file_path = file.file_path
+    if file_path is None:
+        return
     file_name = file_path.split("/")[-1]
 
     my_object = None
-    file_to_upload = await message.bot.download_file(file_path, my_object)
+    file_to_upload = await global_vars.bot.download_file(file_path, my_object)
 
+    if file_to_upload is None:
+        return
     data_file = await MoodleAPI.upload_file(file_to_upload, file_name, user.api_token)
     item_id = data_file[0]["itemid"]
     result = await MoodleAPI.save_submission(token=user.api_token, assign_id=assign.assign_id, item_id=item_id)
@@ -395,7 +462,7 @@ async def submit_assign_file(message: types.Message, state: FSMContext):
         await message.answer(
             f"[{escape_md(course.name)}]({escape_md(url_to_course)})\n[{escape_md(assign.name)}]({escape_md(url_to_assign)})\n\nFile submitted\!",
             reply_markup=add_delete_button().as_markup(),
-            parse_mode=types.ParseMode.MARKDOWN_V2,
+            parse_mode=ParseMode.MARKDOWN_V2,
         )
     else:
         if isinstance(result, list):
@@ -409,23 +476,25 @@ async def submit_assign_file(message: types.Message, state: FSMContext):
                 reply_markup=add_delete_button().as_markup(),
             )
 
-    async with state.proxy() as data:
-        await delete_msg(data["msg"], message)
-    await state.finish()
+    data = await state.get_data()
+    await delete_msg(data["msg"], message)
+    await state.clear()
 
 
 @rate_limit(limit=RATE)
 @count_active_user
 @login_required
 async def submit_assign_text(message: types.Message, state: FSMContext):
+    if message.from_user is None:
+        return
     user_id = message.from_user.id
     user = await UserDB.get_user(user_id)
     if not user:
         return
 
-    async with state.proxy() as data:
-        course_id = data["course_id"]
-        assign_id = data["assign_id"]
+    data = await state.get_data()
+    course_id = data["course_id"]
+    assign_id = data["assign_id"]
 
     courses = await CourseDB.get_courses(user_id, True)
     course = courses[str(course_id)]
@@ -439,14 +508,14 @@ async def submit_assign_text(message: types.Message, state: FSMContext):
         await message.answer(
             f"[{escape_md(course.name)}]({escape_md(url_to_course)})\n[{escape_md(assign.name)}]({escape_md(url_to_assign)})\n\Text submitted\!",
             reply_markup=add_delete_button().as_markup(),
-            parse_mode=types.ParseMode.MARKDOWN_V2,
+            parse_mode=ParseMode.MARKDOWN_V2,
         )
     else:
         await message.answer(f"Error: {result[0]['message']}", reply_markup=add_delete_button().as_markup())
 
-    async with state.proxy() as data:
-        await delete_msg(data["msg"], message)
-    await state.finish()
+    data = await state.get_data()
+    await delete_msg(data["msg"], message)
+    await state.clear()
 
 
 @rate_limit(limit=RATE)
@@ -454,6 +523,8 @@ async def submit_assign_text(message: types.Message, state: FSMContext):
 @Logger.log_msg
 @login_required
 async def update(message: types.Message):
+    if message.from_user is None:
+        return
     user_id = message.from_user.id
 
     await insert_user(user_id)
@@ -466,6 +537,8 @@ async def update(message: types.Message):
 @Logger.log_msg
 @login_required
 async def check_finals(message: types.Message):
+    if message.from_user is None:
+        return
     user_id = message.from_user.id
     if not await CourseDB.is_ready_courses(user_id):
         text = "Your courses are not ready, you are in queue, try later. If there will be some error, we will notify"
